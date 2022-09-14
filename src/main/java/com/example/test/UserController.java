ce2f46389ad0c40c7aebedc5d82bc3ce52169598
daipd <daipd@kaopiz.com>
2022-09-14 13:53:12 +0700

    add User controller

---
+++ src/main/java/com/example/test/UserController.java
@@ -0,0 +1,10 @@
+package com.example.test;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+}
